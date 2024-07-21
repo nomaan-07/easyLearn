aa656078ca03e53088330e087efce6c05d23bb53
Nomaan <nomaan07.dev@gmail.com>
2024-07-22 00:09:43 +0330

    Create client in supabase

--- js/api.js
+++ js/api.js
@@ -1,1 +1,6 @@
+import '../vendor/supabase/supabase-js@2.js';
 
+const { createClient } = supabase;
+const supabaseUrl = 'https://vqvbwalqiwdeyzuiltqm.supabase.co';
+const supabasePublicAPIKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZxdmJ3YWxxaXdkZXl6dWlsdHFtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjE1NTI0MTQsImV4cCI6MjAzNzEyODQxNH0.gx9vDTTyesP9rVv-AiqiY8f4v970X6lzHoQ4nBlacJQ';
+const client = createClient(supabaseUrl, supabasePublicAPIKey);
