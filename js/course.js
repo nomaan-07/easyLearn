337bf394aa12ca072f3421f7bf8ebd74f569373f
Nomaan <nomaan07.dev@gmail.com>
2024-07-26 00:58:16 +0330

    Feat: Commnts section design complete

--- js/course.js
+++ js/course.js
@@ -3,9 +3,18 @@ import './header.js';
 import './change-theme.js';
 
 const headlinesTitleElem = document.querySelectorAll('.headline__title');
+const addNewCommentBtn = document.querySelector('.new-comment-btn');
+const newCommentWrapper = document.querySelector('.new-comment-wrapper');
+const newCommentTextarea = document.querySelector('#new-comment-textarea');
+const newCommentSubmitBtn = document.querySelector('#new-comment-submit-btn');
+const newCommentCloseBtn = document.querySelector('#new-comment-cancel-btn');
+const likeButtons = document.querySelectorAll('.like-btn');
+const openResponseButtons = document.querySelectorAll('.open-response-btn');
+const closeResponseBtn = document.querySelectorAll('.response-comment-cancel-btn');
+const responseCommentWrappers = document.querySelectorAll('.response-comment-wrapper');
+const responseCommentSubmitButtons = document.querySelectorAll('.response-comment-submit-btn');
 
 // toggle headline
-
 const toggleHeadLine = (titleElem) => {
   let totalHeadlineBodyHeight = 0;
 
@@ -31,10 +40,92 @@ const toggleHeadLine = (titleElem) => {
   }
 };
 
+const getCommentID = (el) => {
+  const comment = el.closest('.comment');
+  const commentID = comment.getAttribute('id');
+  return commentID;
+};
+
+const toggleTextarea = (btn = false, openTextarea = false) => {
+  let wrapper = newCommentWrapper;
+  let textarea = newCommentTextarea;
+  if (btn) {
+    const commentID = getCommentID(btn);
+    wrapper = document.querySelector(`#response-wrapper-${commentID}`);
+    textarea = document.querySelector(`#response-textarea-${commentID}`);
+  }
+
+  if (openTextarea) {
+    wrapper.classList.remove('max-h-0');
+    wrapper.classList.remove('overflow-hidden');
+    textarea.focus();
+  } else {
+    textarea.value = '';
+    wrapper.classList.add('max-h-0');
+    wrapper.classList.add('overflow-hidden');
+  }
+};
+
+// FIXME: Send Comment ot database
+const submitComment = (textarea, wrapper) => {
+  const comment = textarea.value.trim();
+  toggleTextarea();
+  // TODO: modal comment submit
+};
+
+const textareaAutoResize = (event) => {
+  event.target.style.height = '160px';
+  event.target.style.height = `${event.target.scrollHeight}px`;
+};
+
+const addLike = (svg, el, count) => {
+  svg.innerHTML = '<use href="#heart"></use>';
+  el.innerText = count + 1;
+};
+
+const reduceLike = (svg, el, count) => {
+  svg.innerHTML = '<use href="#heart-outline"></use>';
+  el.innerText = count - 1;
+};
+
+// FIXME: add to database instead of localStorage
+const toggleLike = (btn, isLoading = false) => {
+  const commentID = getCommentID(btn);
+  const isLiked = localStorage.getItem(`isLiked-${commentID}`) === 'true';
+  const likeElem = btn.children[1];
+  const svg = btn.children[0];
+  const likeCount = Number(likeElem.innerText);
+  if (isLoading) {
+    isLiked ? addLike(svg, likeElem, likeCount) : null;
+  } else {
+    if (isLiked) {
+      localStorage.setItem(`isLiked-${commentID}`, false);
+      reduceLike(svg, likeElem, likeCount);
+    } else {
+      localStorage.setItem(`isLiked-${commentID}`, true);
+      addLike(svg, likeElem, likeCount);
+    }
+  }
+};
+
 headlinesTitleElem.forEach((titleElem) =>
   titleElem.addEventListener('click', () => {
     toggleHeadLine(titleElem);
   })
 );
 
+likeButtons.forEach((btn) => {
+  toggleLike(btn, true);
+});
+
+likeButtons.forEach((btn) => btn.addEventListener('click', () => toggleLike(btn)));
+
+addNewCommentBtn.addEventListener('click', () => toggleTextarea(false, true));
+openResponseButtons.forEach((btn) => btn.addEventListener('click', () => toggleTextarea(btn, true)));
+closeResponseBtn.forEach((btn) => btn.addEventListener('click', () => toggleTextarea(btn)));
+
+newCommentCloseBtn.addEventListener('click', () => toggleTextarea());
+newCommentSubmitBtn.addEventListener('click', () => submitComment(newCommentTextarea, newCommentWrapper));
+
+newCommentTextarea.addEventListener('input', textareaAutoResize);
 window.addEventListener('load', removeLoader);
