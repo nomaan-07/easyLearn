3d57c98f00b6b9a4f3077782c27d814e1ef080e7
Nomaan <nomaan07.dev@gmail.com>
2024-08-13 04:22:38 +0330

    Feat: 404 page

---
+++ js/404.js
@@ -0,0 +1,6 @@
+import { favIcon, localStorageTheme } from './dom-elements.js';
+
+if (localStorageTheme) {
+  document.documentElement.className = `scroll-smooth ${localStorageTheme}`;
+  favIcon.href = `images/favIcons/${localStorageTheme}-favicon-64x64.png`;
+}
